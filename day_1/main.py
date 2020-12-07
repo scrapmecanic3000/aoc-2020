4bd0813bf46f7326d7c66ed896de2454249906f2
MichaelBozhanov <scrapmecanic3000@gmail.com>
2020-12-07 17:23:51 +0200

    first task done

--- day_1/main.py
+++ day_1/main.py
@@ -1,7 +1,14 @@
-
+def sum_of_pair(_input, _sum):
+    for i in _input:
+        for j in _input:
+            if i + j == _sum:
+                return i, j
+            if i == j:
+                continue
 
 with open("./input_data.txt", "r") as input_file:
     input_data = input_file.read()
 
-
-
+_input = [int(i) for i in input_data.split("\n")]
+i, j = sum_of_pair(_input, 2020)
+print(i*j)
