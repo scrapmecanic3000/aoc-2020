e818507a161b375b4922e3e0bc10bddc3fe7e0b2
MichaelBozhanov <scrapmecanic3000@gmail.com>
2020-12-07 17:29:45 +0200

    repositioned number equity checking

--- day_1/main.py
+++ day_1/main.py
@@ -1,10 +1,11 @@
 def sum_of_pair(_input, _sum):
     for i in _input:
         for j in _input:
-            if i + j == _sum:
-                return i, j
             if i == j:
                 continue
+            if i + j == _sum:
+                return i, j
+
 
 with open("./input_data.txt", "r") as input_file:
     input_data = input_file.read()
