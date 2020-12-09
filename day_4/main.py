64c06c785829be68d06fa59be71ef1582173cf8e
MichaelBozhanov <scrapmecanic3000@gmail.com>
2020-12-09 02:21:16 +0200

    finally done this shit aka fourth day completed

--- day_4/main.py
+++ day_4/main.py
@@ -9,28 +9,20 @@ def convert_to_dict(a):
 
 def is_valid(passport):
     is_valid = False
-
-    if len(passport.keys()) == 8:   
-        if 1920 <=  int(passport["byr"]) <= 2002 and 2010 <= int(passport["iyr"]) <= 2020 and 2020 <= int(passport["eyr"]) <= 2030 and ((bool(re.search("cm", passport["hgt"])) and int(re.split("cm", passport["hgt"])[0]) in range(150,193)) or (bool(re.search("[in]",passport["hgt"])) and int(re.split("in", passport["hgt"])[0]) in range(59, 76))) and bool(re.search("#([a-f]|[0-9]){6}", passport["hcl"])) and passport["ecl"] in ["amb","blu","brn","gry","grn","hzl","oth"] and bool(re.search("^([0-9]{9})$", passport["pid"])):
-            return True
-        else:
-            return False
-    elif len(passport.keys()) == 7:
-        try:
-            if 1920 <=  int(passport["byr"]) <= 2002 and 2010 <= int(passport["iyr"]) <= 2020 and 2020 <= int(passport["eyr"]) <= 2030 and ((bool(re.search("cm", passport["hgt"])) and int(re.split("cm", passport["hgt"])[0]) in range(150,193)) or (bool(re.search("[in]",passport["hgt"])) and int(re.split("in", passport["hgt"])[0]) in range(59, 76))) and bool(re.search("#([a-f]|[0-9]){6}", passport["hcl"])) and passport["ecl"] in ["amb","blu","brn","gry","grn","hzl","oth"] and bool(re.search("^([0-9]{9})$", passport["pid"])):
-                return True
-            else:
-                return False
-        except KeyError:
-            return False
-    else:
+    try:
+        return 1920 <= int(passport["byr"]) <= 2002 \
+            and 2010 <= int(passport["iyr"]) <= 2020 \
+            and 2020 <= int(passport["eyr"]) <= 2030 \
+            and passport["hcl"][0] == "#" and len(passport["hcl"][1:]) == 6 \
+            and ((passport["hgt"][-2:] == "cm" and int(passport["hgt"][:-2]) in range(150, 193+1)) \
+            or  (passport["hgt"][-2:] == "in" and int(passport["hgt"][:-2]) in range(59, 76+1))) \
+            and passport["ecl"] in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"] \
+            and len(passport["pid"])==9 
+    except KeyError:
         return False
-    
-    return False
 
 
-
-with open("c:/Users/Lenovo T440/Desktop/aoc-2020/day_4/input_data.txt", "r") as input_file:
+with open("./input_data.txt", "r") as input_file:
     input_data = input_file.read()
 
 credentials_raw = input_data.split("\n\n")  # split our dat into passports
